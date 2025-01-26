95a4ef0cb6b4282a83047ef471b9c67f9a079c6e
Chris Fredricksen <cmfredricksen@proton.me>
2025-01-25 19:55:17 -0600

    Initial commit

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,15 @@
+<section class="hero">
+	<div class="hero-showcase text-center bold">
+		<h1 class="purple">READ THE CARDS</h1>
+		<h1 class="purple">FOR FUN & ENRICHMENT</h1>
+		<h4 class="italic semi-bold">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, corporis!
+		</h4>
+	</div>
+
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae possimus, praesentium illum
+		voluptatem id soluta. Autem unde quia vitae? Dolor asperiores distinctio expedita itaque nulla
+		minima beatae quaerat ad doloribus.
+	</p>
+</section>
