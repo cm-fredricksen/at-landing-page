e8efc1197d1c4e4b2e5a2d39cbfd32eabf2f2abb
Chris Fredricksen <cmfredricksen@proton.me>
2025-01-30 17:08:05 -0600

    add title and favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
 	import { HeroSection, PreviewSection, AuthorSection, FaqSection } from '$components';
 </script>
 
+<svelte:head>
+	<title>Agnostic Tarot &clubs;</title>
+</svelte:head>
+
 <HeroSection />
 <PreviewSection />
 <AuthorSection />
